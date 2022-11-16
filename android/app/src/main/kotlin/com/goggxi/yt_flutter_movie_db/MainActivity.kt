9fe20e217f52ff93ec0f90129466fe8675e5c10d
goggxi <goggxi@gmail.com>
2022-11-16 13:13:08 +0800

    initial and setup project setup splash screen

---
+++ android/app/src/main/kotlin/com/goggxi/yt_flutter_movie_db/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.goggxi.yt_flutter_movie_db
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String imageUrlW500 = 'https://image.tmdb.org/t/p/w500';
+  static const String imageUrlOriginal = 'https://image.tmdb.org/t/p/original';
+  static const String apiKey = 'd8e957a69af1be921c9c4466f5661e87';
+
+}
