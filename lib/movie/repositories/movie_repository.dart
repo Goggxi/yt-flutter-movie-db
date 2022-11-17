dd3a4c0061e4aa5622d50ab13570c30ffb518a4f
goggxi <goggxi@gmail.com>
2022-11-17 08:27:59 +0800

    Revert "implement discover movies api"
    This reverts commit a197a686275fc0f7e2ed7b159377c59266619937.

--- lib/movie/repositories/movie_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:yt_flutter_movie_db/movie/models/movie_model.dart';
-
-abstract class MovieRepository {
-  Future<Either<String, MovieResponseModel>> getDiscover({int page = 1});
-}
